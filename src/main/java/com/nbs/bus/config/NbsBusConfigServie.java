878726b2be8c8b0432b39729a7e938996b5cbe60
gajanan@symbiotiktech.com <gajanan@symbiotiktech.com>
2023-12-20 15:49:14 +0530

    initial commit

---
+++ src/main/java/com/nbs/bus/config/NbsBusConfigServie.java
@@ -0,0 +1,16 @@
+package com.nbs.bus.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class NbsBusConfigServie {
+
+
+}
